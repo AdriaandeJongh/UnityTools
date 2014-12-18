41e1ee10d91b85fd00b49572c28ea762eb7c06eb
Adriaan <hello@adriaandejongh.com>
2014-12-18 12:56:43 +0100

    added simple script for dynamic position animation

---
+++ DynamicPositionAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicPositionAnimation : MonoBehaviour 
+{
+	public float animatableValue = 0f;
+
+	private Vector3 from;
+	private Vector3 to;
+	private bool animating;
+
+	public void AnimateFromTo(Vector3 _from, Vector3 _to)
+	{
+		from = _from;
+		to = _to;
+		animating = true;
+		GetComponent<Animator>().Play("Move");
+	}
+
+	void Update()
+	{
+		if(animating)
+			transform.position = Vector3.Lerp(from, to, animatableValue);
+	}
+}
