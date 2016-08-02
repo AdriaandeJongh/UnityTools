7b8d16914dc8f5eb543e2b55e6fe4cd2c9f5dfa4
Adriaan <hello@adriaandejongh.com>
2016-08-02 20:03:47 +0200

    added two more attributes I use occasionally and merged the attributes and drawers

---
+++ Attributes/HideWhenFalse.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class HideWhenFalse : PropertyAttribute 
+{
+	public readonly string hideBoolean;
+
+	public HideWhenFalse (string booleanName)
+	{
+		this.hideBoolean = booleanName;
+	}
+}
+
+#if UNITY_EDITOR
+[CanEditMultipleObjects]
+[CustomPropertyDrawer(typeof(HideWhenFalse))]
+public class HideWhenFalseDrawer : PropertyDrawer 
+{
+
+	public override void OnGUI ( Rect position, SerializedProperty property, GUIContent label) 
+	{
+		HideWhenFalse hiddenAttribute = attribute as HideWhenFalse;
+		SerializedProperty boolProperty = property.serializedObject.FindProperty(hiddenAttribute.hideBoolean);
+
+		if(boolProperty.boolValue)
+			EditorGUI.PropertyField(position, property, label, true);
+	}
+
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		HideWhenFalse hiddenAttribute = attribute as HideWhenFalse;
+		SerializedProperty boolProperty = property.serializedObject.FindProperty(hiddenAttribute.hideBoolean);
+
+		if(!boolProperty.boolValue)
+			return 0f;
+
+		return EditorGUI.GetPropertyHeight(property);
+	}
+}
+#endif
