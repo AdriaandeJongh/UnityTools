16684aa3cfe3dd609535248fe6971b9691ab57e3
Adriaan <hello@adriaandejongh.com>
2016-10-05 15:30:54 +0200

    made cameracontrols work with the latest version of inp.ut

--- CameraControls.cs
+++ CameraControls.cs
@@ -1,19 +1,13 @@
-﻿// Only works together with Inp.ut
-// By Adriaan de Jongh, http://adriaandejongh.nl
-// More info & other Unity scripts: https://github.com/AdriaandeJongh/UnityTools
-
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.EventSystems;
 
 public class CameraControls : MonoBehaviour 
 {
-	public static CameraControls instance;
-
-	private float minimumOrthographicSize = 4.0f;
-	private float maximumOrthographicSize = 7.0f;
+	public static float minimumOrthographicSize = 3.5f;
+	public static float maximumOrthographicSize = 7.0f;
 	private float wantedOrthographicSize = 4.0f;
 
 	private Camera cam;
@@ -21,37 +15,94 @@ public class CameraControls : MonoBehaviour
 	private Bounds cameraBounds = new Bounds(Vector3.zero, Vector3.one);
 
 	private bool waitForInteraction = false;
-
-	void Awake()
-	{
-		instance = this;
-	}
+	private bool controlsEnabled = true;
 
 	void Start ()
 	{
-		cam = Camera.main;
-		wantedOrthographicSize = cam.orthographicSize;
-
-		Application.targetFrameRate = 60;
-
+		cam = GetComponent<Camera>();
+		
+		//determine the camera bounds
 		foreach (Renderer r in FindObjectsOfType(typeof(Renderer)))
 		{
 			if(r.gameObject.layer != 5) //ignore the characters in the UI!
 				cameraBounds.Encapsulate(r.bounds);
 		}
+
+		//zoom into the level if the level is small
+		if(cameraBounds.extents.y < maximumOrthographicSize)
+		{
+			cam.orthographicSize = Mathf.Clamp(cameraBounds.extents.y, minimumOrthographicSize, maximumOrthographicSize);
+		}
+
+		wantedOrthographicSize = cam.orthographicSize;
+
+		//contract the bounds so that we have less white space
+		cameraBounds.Expand(-3f);
+		
+		//but make sure we don't make it toooo small...
+		cameraBounds.Encapsulate(Vector3.up);
+		cameraBounds.Encapsulate(Vector3.down);
+		cameraBounds.Encapsulate(Vector3.left);
+		cameraBounds.Encapsulate(Vector3.right);
 	}
 
 	void Update () 
-	{
+	{}
 		if(Inp.ut.isOverUI)
 			waitForInteraction = true;
 
-		if(waitForInteraction && !Inp.ut.anyInput)
+		if(waitForInteraction && !Inp.ut.any)
 			waitForInteraction = false;
 
-		if(!Inp.ut.isOverUI && !waitForInteraction)
+		if(!waitForInteraction && controlsEnabled)
 		{
-			if(Application.isMobilePlatform) 
+			if(Inp.ut.mouseEnabled)
+			{
+				if(Input.mouseScrollDelta.y != 0f) //we are scrolling (and reposittioning at the same time)!
+				{
+					//calc pos before zooming
+					Vector3 posBeforeZoom = Inp.ut.To2DWorld(Inp.ut.Position(Inp.mouseId));
+					
+					//save orth before
+					float camOrth = cam.orthographicSize;
+					
+					//calc orth wanted
+					wantedOrthographicSize = Mathf.Clamp(cam.orthographicSize - Input.mouseScrollDelta.y * 0.2f, minimumOrthographicSize, maximumOrthographicSize);
+					
+					//set orth wanted
+					cam.orthographicSize = wantedOrthographicSize;
+					
+					//calc pos after
+					Vector3 posAfterZoom = Inp.ut.To2DWorld(Inp.ut.Position(Inp.mouseId));
+					
+					//reset orth
+					cam.orthographicSize = camOrth;
+					
+					if(posBeforeZoom != posAfterZoom)
+					{
+						//set the position directly here, because we don't really want inertia...
+						Vector3 insideBoundsAfterZoom = cameraBounds.ClosestPoint(cam.transform.position + (posBeforeZoom - posAfterZoom));
+						
+						cam.transform.position = insideBoundsAfterZoom + Vector3.back * 10f;
+					}
+				}
+				
+				
+				if(!Input.GetMouseButtonDown(0) && Input.GetMouseButton(0))
+					inertia = Inp.ut.To2DWorld(Inp.ut.PreviousPosition(Inp.mouseId)) - Inp.ut.To2DWorld(Inp.ut.Position(Inp.mouseId));
+				else if(Input.GetMouseButtonUp(0))
+				{
+					Vector2 deltaSample = Inp.ut.DeltaPositionSample(Inp.mouseId, 3);
+					
+					if(deltaSample != Vector2.zero)
+					{
+						inertia = Inp.ut.To2DWorld(Inp.ut.Position(Inp.mouseId) - deltaSample) - Inp.ut.To2DWorld(Inp.ut.Position(Inp.mouseId));
+					}
+				}
+				
+			}
+			
+			if(Inp.ut.touchEnabled) 
 			{
 				if(Input.touchCount == 1)
 				{
@@ -59,22 +110,26 @@ public class CameraControls : MonoBehaviour
 
 					if(touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended)
 					{
-						//Only sample when we've let go, so that there is no delay!
-
-						Vector2 currentPosition = Inp.ut.TouchPosition(touch.fingerId);
-						Vector2 deltaSample = Inp.ut.TouchDeltaPositionSample(touch.fingerId, 3);
-
+						Vector2 currentPosition = Inp.ut.Position(touch.fingerId);
+						Vector2 deltaSample = Inp.ut.DeltaPositionSample(touch.fingerId, 3);
+						
 						if(deltaSample != Vector2.zero)
 						{
 							//if we're not standing still...
 							Vector2 sampledPrevPos = currentPosition - deltaSample;
-							inertia = Inp.ut.To2DWorld(sampledPrevPos) - Inp.ut.To2DWorld(Inp.ut.TouchPosition(touch.fingerId));
+							inertia = Inp.ut.To2DWorld(sampledPrevPos) - Inp.ut.To2DWorld(Inp.ut.Position(touch.fingerId));
 						}
 					}
 					else
 					{
-						inertia = Inp.ut.To2DWorld(Inp.ut.TouchPreviousPosition(touch.fingerId))
-							- Inp.ut.To2DWorld(Inp.ut.TouchPosition(touch.fingerId));
+						inertia = Inp.ut.To2DWorld(Inp.ut.PreviousPosition(touch.fingerId))
+							- Inp.ut.To2DWorld(Inp.ut.Position(touch.fingerId));
+					}
+
+					//handle all the special cases for tvOS
+					if(Application.platform == RuntimePlatform.tvOS)
+					{
+						inertia *= -0.3f;
 					}
 				} 
 				else if(Input.touchCount >= 2)
@@ -83,8 +138,8 @@ public class CameraControls : MonoBehaviour
 					int id1 = Input.GetTouch(1).fingerId;
 
 					// Find the magnitude (distance) of the vector between the touches in the current & previous frame.
-					float prevTouchDeltaMag = (Inp.ut.TouchPreviousPosition(id0) - Inp.ut.TouchPreviousPosition(id1)).magnitude;
-					float touchDeltaMag = (Inp.ut.TouchPosition(id0) - Inp.ut.TouchPosition(id1)).magnitude;
+					float prevTouchDeltaMag = (Inp.ut.PreviousPosition(id0) - Inp.ut.PreviousPosition(id1)).magnitude;
+					float touchDeltaMag = (Inp.ut.Position(id0) - Inp.ut.Position(id1)).magnitude;
 					
 					// Find the difference in the distances between each frame.
 					float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
@@ -103,14 +158,11 @@ public class CameraControls : MonoBehaviour
 						wantedOrthographicSize += deltaMagnitudeDiff * 0.008f;
 					}
 
-					//set camera!
-					cam.orthographicSize = wantedOrthographicSize;
-
 					//aaand set inertia so that the camera moves according to the average of both touches
 					//(in the rare case that you let go of two touches in one frame there might be a halt if the 
 					// last frame reports no delta position, because no sampling happens here... oh well :)
-					inertia = Inp.ut.To2DWorld((Inp.ut.TouchPreviousPosition(id0) + Inp.ut.TouchPreviousPosition(id1)) / 2f)
-						- Inp.ut.To2DWorld((Inp.ut.TouchPosition(id0) + Inp.ut.TouchPosition(id1)) / 2f);
+					inertia = Inp.ut.To2DWorld((Inp.ut.PreviousPosition(id0) + Inp.ut.PreviousPosition(id1)) / 2f)
+						- Inp.ut.To2DWorld((Inp.ut.Position(id0) + Inp.ut.Position(id1)) / 2f);
 
 				}
 
@@ -129,23 +181,18 @@ public class CameraControls : MonoBehaviour
 				}
 
 			}
-			else
-			{
-				cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - Input.GetAxis("Mouse ScrollWheel"), minimumOrthographicSize, maximumOrthographicSize);
-
-
-				if(Input.GetMouseButton(0))
-					inertia = Inp.ut.To2DWorld(Inp.ut.mousePreviousPosition) - Inp.ut.To2DWorld(Inp.ut.mousePosition);
-				else if(Input.GetMouseButtonUp(0))
-					inertia = Inp.ut.To2DWorld(Inp.ut.mousePosition - Inp.ut.MouseDeltaPositionSample(3)) - Inp.ut.To2DWorld(Inp.ut.mousePosition);
-
-			}
+			
+			
+			cam.orthographicSize = wantedOrthographicSize;
+			
 		}
 
-		if(!Inp.ut.anyInput || Inp.ut.isOverUI || waitForInteraction)
+		if(!Inp.ut.any || Inp.ut.isOverUI || waitForInteraction)
 			inertia *= 0.9f;
 
-		cam.transform.position = cameraBounds.ClosestPoint(cam.transform.position + inertia);
+		Vector3 insideBounds = cameraBounds.ClosestPoint(cam.transform.position + inertia);
+
+		cam.transform.position = insideBounds + Vector3.back * 10f;
 	}
 
 	public void WaitForInteraction()
@@ -153,4 +200,10 @@ public class CameraControls : MonoBehaviour
 		inertia = Vector3.zero;
 		waitForInteraction = true;
 	}
+
+	public void SetControllable (bool controllable)
+	{
+		controlsEnabled = controllable;
+	}
+
 }
