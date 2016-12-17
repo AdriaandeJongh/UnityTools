3d40c8661948d89b627af75bfbd6089742f0399d
Adriaan <hello@adriaandejongh.com>
2016-12-17 13:59:04 +0100

    allowing actual multi-target editting with the inspector button

--- Attributes/InspectorButton.cs
+++ Attributes/InspectorButton.cs
@@ -3,6 +3,7 @@
 using UnityEditor;
 #endif
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -19,20 +20,28 @@ public class InspectorButtonDrawer : Editor
 	public override void OnInspectorGUI()
 	{
 		base.OnInspectorGUI();
-
+		
 		var mono = target as MonoBehaviour;
-
+		
 		var methods = mono.GetType()
-			.GetMembers(BindingFlags.Instance | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
-				BindingFlags.NonPublic)
+			.GetMembers(BindingFlags.Instance | 
+			BindingFlags.Instance | 
+			BindingFlags.Static | 
+			BindingFlags.Public |
+			BindingFlags.NonPublic)
 			.Where(o => System.Attribute.IsDefined(o, typeof (InspectorButton)));
-
+		
 		foreach (var memberInfo in methods)
 		{
 			if (GUILayout.Button(memberInfo.Name))
 			{
 				var method = memberInfo as MethodInfo;
-				method.Invoke(mono, null);
+				
+				foreach (var t in targets)
+				{
+					var m = t as MonoBehaviour;
+					method.Invoke(m, null);
+				}
 			}
 		}
 	}
