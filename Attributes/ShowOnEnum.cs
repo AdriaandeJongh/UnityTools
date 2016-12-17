b8d0d4609f7912b8f82517b70b955f77e8746177
Adriaan <hello@adriaandejongh.com>
2016-12-17 14:00:40 +0100

    added ShowOnEnum to only show vars in the inspector when a certain enum was set

---
+++ Attributes/ShowOnEnum.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+
+public class ShowOnEnum : PropertyAttribute 
+{
+	public readonly string enumName;
+	public readonly int wantedValue; 
+	
+	public ShowOnEnum(string enumName, int wantedValue)
+	{
+		this.enumName = enumName;
+		this.wantedValue = wantedValue;
+	}
+}
+
+#if UNITY_EDITOR
+[CanEditMultipleObjects]
+[CustomPropertyDrawer(typeof(ShowOnEnum))]
+public class ShowOnEnumDrawer : PropertyDrawer 
+{
+	
+	public override void OnGUI ( Rect position, SerializedProperty property, GUIContent label) 
+	{
+		ShowOnEnum att = attribute as ShowOnEnum;
+		SerializedProperty enumProperty = property.serializedObject.FindProperty(att.enumName);
+		
+		if(enumProperty.enumValueIndex == att.wantedValue)
+			EditorGUI.PropertyField(position, property, label, true);
+	}
+	
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		ShowOnEnum att = attribute as ShowOnEnum;
+		SerializedProperty enumProperty = property.serializedObject.FindProperty(att.enumName);
+		
+		if(enumProperty.enumValueIndex != att.wantedValue)
+			return -2f;
+		
+		return EditorGUI.GetPropertyHeight(property);
+	}
+}
+#endif
